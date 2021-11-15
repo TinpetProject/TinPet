2483f91c4f5910738f343cdfc9fb1b6ba9c25444
Minh Trí Hoàng <75794170+trihoagmih@users.noreply.github.com>
2021-11-15 23:26:32 +0700

    nothing

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    database: "tinpet",
-    password: "gunny318",
+    database: "",
+    password: "",
     multipleStatements: true,
 });
 
