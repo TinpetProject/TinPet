7ddaf7f4ab1055316b7c2b35015a8bcb990960e1
lengockien2000 <74950189+lengockien2000@users.noreply.github.com>
2022-01-02 23:57:28 +0700

    update

--- portal/src/screen/Setting/Setting.js
+++ portal/src/screen/Setting/Setting.js
@@ -5,8 +5,7 @@ export default function Setting({userID, hideSetting}) {
     return (
         <div className="container">
             <div className="container-layout" onClick={hideSetting}></div>
-            <div className="container-content-box">
-                hahaha
+            <div className="container-content-wrapper">
                 <div></div>
             </div>
         </div>
