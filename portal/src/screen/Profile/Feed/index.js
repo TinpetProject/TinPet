a5432cdb6cd505768a1cce04bb44af4a0a993ba2
Thuy Dung Nguyen <thuydungnguyen.ictk63@gmail.com>
2022-01-03 01:14:03 +0700

    [ADD] display posts and comments from db

--- portal/src/screen/Profile/Feed/index.js
+++ portal/src/screen/Profile/Feed/index.js
@@ -1,30 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Posts } from "../dummyData";
 import Post from "../Post";
 import { FeedWrapper } from "./style";
+import { PostServices } from "../../../services";
 
 export default function Feed({ userID }) {
   const token = localStorage.getItem("token");
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
-    const fetchList = async () => {
-      const result = await fetch(`http://localhost:8888/${userID}/posts/1`, {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          "Content-Type": "application/json",
-          authorization: `Bearer ${token}`,
-        },
-      });
-      console.log(result);
+    const fetchPost = async () => {
+      const data = await PostServices.getPostByUserID(userID, token);
+      console.log(data);
+      if (!!data && data.code === 200) {
+        // console.log("commentList");
+        setPosts(data.data);
+      }
     };
-    token && fetchList();
-  }, []);
+    !!token && fetchPost();
+    // const fetchList = async () => {
+    //   const result = await fetch(`http://localhost:8888/post/${userID}/posts/1`, {
+    //     method: "GET",
+    //     headers: {
+    //       accept: "application/json",
+    //       "Content-Type": "application/json",
+    //       authorization: `Bearer ${token}`,
+    //     },
+    //   });
+    //   console.log(result);
+    // };
+    // token && fetchList();
+  }, [token, userID]);
+  console.log(posts);
   return (
     <div>
       <FeedWrapper>
-        {Posts.map((p) => (
-          <Post key={p.id} post={p} />
-        ))}
+        {!!posts.length > 0 && posts.map((p) => <Post key={p.postID} post={p} />)}
       </FeedWrapper>
     </div>
   );
