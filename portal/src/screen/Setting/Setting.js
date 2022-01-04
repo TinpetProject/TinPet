689fecb1c6c92834f5a1e4eac986817f903fc852
lengockien2000 <74950189+lengockien2000@users.noreply.github.com>
2022-01-04 14:58:56 +0700

    update again

--- portal/src/screen/Setting/Setting.js
+++ portal/src/screen/Setting/Setting.js
@@ -34,24 +34,20 @@ export default function Setting({hideSetting}) {
         //   setSelectedUser(userID);
         // }
         const getUser = async () => {
-          axios
-            .get(`/user/${selectedUser}/profile`)
-            .then((response) => {
-              console.log(response);
-              setUser({
-                ...response.data.data,
-                avatar:
-                  "https://scontent.fhan5-4.fna.fbcdn.net/v/t39.30808-6/270772893_1567946906894523_1047998408474512960_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=UDouyg3f9X4AX-pmsZd&tn=i1yGCvqKaMsUYmLN&_nc_ht=scontent.fhan5-4.fna&oh=00_AT8z-4gaLISuR7xppz5vpNfe01um66ajORsM6f-vM7pKKg&oe=61D5CA10",
-                email:
-                    "minhtam@gmail.com",
-                name: "Minh Tâm",
-              });
-            })
-            .catch((error) => console.log(error));
+            axios
+                .get(`/user/${selectedUser}/profile`)
+                .then((response) => {
+                // console.log(response);
+                setUser({
+                    ...response.data.data,
+                    avatar: response.data.data.avatar || "https://res.cloudinary.com/thecodingpanda/image/upload/v1641272668/zoyndaseei9wnbrybwxr.png?fbclid=IwAR2YOBaBi-4FdUEFD_XjI9vgrwHAcfpupP8vnGS7p26Lrq8v3XGzFvD3pxk",
+                });
+                })
+                .catch((error) => console.log(error));
         };
-    
         selectedUser && getUser();
-      }, [selectedUser]);
+    }, [selectedUser]);
+
     const mainMenu = [
         {
             icon: <Icon className="menu__account-icon" icon="mdi:account-circle-outline" />,
@@ -95,7 +91,6 @@ export default function Setting({hideSetting}) {
         }
     ];
 
-    
     return (
         <div className="container">
             <div className="container-layout" onClick={hideSetting}></div>
